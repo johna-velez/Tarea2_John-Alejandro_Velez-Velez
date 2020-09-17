e71e50824f246c16062575b843da31f1ad9fb23d
johna-velez <70598373+johna-velez@users.noreply.github.com>
2020-09-16 23:55:54 -0500

    Add files via upload

---
+++ clases.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from math import sqrt
+
+class vectorcartesiano:
+    def __init__(self,x,y,z):
+        self.x=float(x)
+        self.y=float(y)
+        self.z=float(z)
+        self.modulo=sqrt(x**2+y**2+z**2) #encuentra el modulo del vector
+        
+    def cruz(self, b):   # producto cruz
+        cordx=self.y*b.z-self.z*b.y
+        cordy=self.z*b.x-self.x*b.z
+        cordz=self.x*b.y-self.y*b.x
+        
+        r=vectorcartesiano(cordx,cordy,cordz)
+        return r
+    
+    def __mul__(self, v): #producto interno, sobrecargando *
+        cordx=self.x*v.x
+        cordy=self.y*v.y
+        cordz=self.z*v.z
+        r=cordx+cordy+cordz
+        
+        return r
+    
+    def __add__(self, v): #suma de vectores, sobrecargando +
+        cordx=self.x+v.x
+        cordy=self.y+v.y
+        cordz=self.z+v.z
+        
+        r=vectorcartesiano(cordx,cordy,cordz)
+        return r
+    
+    def __sub__(self, v): #resta de vectores, sobrecargando -
+        cordx=self.x-v.x
+        cordy=self.y-v.y
+        cordz=self.z-v.z
+        r=vectorcartesiano(cordx,cordy,cordz)
+        return r
+    
+    def __eq__(self, v): # compara las componentes de los vectores para ver si
+        if self.x-v.x==0 and self.y-v.y==0 and self.z-v.z==0: # son iguales o diferentes
+            return 'los vectores son iguales'
+        else:
+            return 'los vectores son diferentes'
+        
+    ''''def __repr__ (self): ##para mirar la representacion
+        
+        return '<Vector (%f, %f, %f)>' % (self.x, self.y, self.z'''
+    
+    
+
+
+# In[237]:
+
+
+class vectorpolar(vectorcartesiano):
+    """"def __ini__(self, r, theta, phi):
+        self.r=r
+        self.theta=theta
+        self.phi=phi
+        self.modulo=3"""
+    #def conv(self):
+        
+
+
+# In[238]:
+
+
+"""y=vectorcartesiano(1,2,3)
+yp=vectorpolar(-7,2,0)
+
+print(y.__dict__)
+print(yp.__dict__)"""
+
+
+# In[190]:
+
+
+1==2
+
+
+# In[ ]:
+
+
+
+
